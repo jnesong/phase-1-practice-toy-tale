e66ff13847429df317d6fb5671d06e072f7893ac
Jenny Chau <jnechau@gmail.com>
2021-11-09 00:29:16 -0500

    added all toy cards, added like button/counter, need to fix form

--- src/index.js
+++ src/index.js
@@ -12,37 +12,62 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+  
+//your code here
 
-let form = document.querySelector(".add-toy-form")
-    form.addEventListener('submit', (e)=> {
-        e.preventDefault()
-        addToy4(e.target.value)
-        form.reset()
-    })
+  let form = document.querySelector(".add-toy-form")
+    form.addEventListener("submit", (e) => {
+      e.preventDefault()
+      //let toyName = form.querySelector(".input-text").value
+     // console.log(toyName)
+      postTheToys(e.target.value)
+      form.reset()
+    })//end of form eventListener 
 
-const addToy4 = function(){
-    fetch("http://localhost:3000/toys/4")
-        .then (function(resp){
-            return resp.json()
-        })
-        .then (function (json){
-            console.log(json)
-            let toySlinkyDog = document.querySelector("#toy-collection")
-            let toyName = document.createElement("h2")
-            toyName = json["name"]
-            toySlinkyDog.append(toyName)
-            toySlinkyDog.id = json["id"] // this sets the id number
-            console.log(toySlinkyDog)
-            // the above code gets the toy name to be fetched... only if it its c/p'd into the console...
-            let toyImage = document.createElement("img")
-            toyImage.src = json["image"]
-            toySlinkyDog.append(toyImage)
-            // this adds the toy's image with the same issue of needing to c/p code into console...
-            let toyLikes = document.createElement("p")
-            toyLikes = json["likes"] + "  likes"
-            toySlinkyDog.append(toyLikes)
-        })
-}  
+  let arrayToys = []//creating an empty array to fill with json data
+
+  const postTheToys = function(){
+      fetch("http://localhost:3000/toys")
+      .then ((resp) => resp.json())
+      .then ((json) => {
+        console.log(json)
+        let arrayToys = json
+        console.log (arrayToys)
+
+        const makeToyCard = function(toy){ // don't forget to put toy/item as a parameter!
+          let toyCard = document.createElement("div")
+          toyCard.className = "card"
+          let toyCollection = document.querySelector("#toy-collection")
+          let toyHeader = document.createElement("h2")
+          toyHeader.textContent = toy["name"]
+          console.log(toyHeader)
+          let toyImage = document.createElement("img")
+          toyImage.src = toy["image"]
+          toyImage.className = "toy-avatar"
+          let toyLikes = document.createElement("p")
+          toyLikes.textContent = toy["likes"] + " Likes" //don't forget .textContent here
+          let toyButton = document.createElement("button")
+          toyButton.textContent = "Like <3"
+          toyButton.className = "like-btn"
+          toyButton.id = toy["id"]
+          toyButton.addEventListener("click", ()=>{
+            toyLikes.textContent = ++toy["likes"] + " likes"
+          })//end of toyButton event listener
 
 
-});
+          //append to toy collection
+          toyCard.append(toyHeader, toyImage, toyLikes, toyButton,)
+          toyCollection.append(toyCard)
+          
+        }// end of makeToyCard function
+
+        arrayToys.forEach(makeToyCard);
+
+        
+  
+      })//end of second .then()
+
+    }//end of postTheToys function
+    
+
+});// this is the end curly brace of the DOMContentLoaded event listener
